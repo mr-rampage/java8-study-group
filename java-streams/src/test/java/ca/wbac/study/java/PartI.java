e457062b7867bc82656c937281e4becb63a492f4
mr-rampage <readonly@che.wbac.ca>
2017-01-16 21:24:17 -0500

    initial com,mit

---
+++ java-streams/src/test/java/ca/wbac/study/java/PartI.java
@@ -0,0 +1,13 @@
+package ca.wbac.study.java;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.Test;
+
+public class PartI 
+{
+    @Test
+    public void testI()
+    {
+    }
+}
